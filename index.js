42f63c7fdc5c31f3ba7ca03c0c17880d2d7c7bcf
marcin93w <marcin93w@gmail.com>
2016-05-21 19:54:46 +0200

    Example server code

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 var bleno = require('bleno');
 
+var BlenoPrimaryService = bleno.PrimaryService;
+var Characteristic = require('./characteristic');
+
 bleno.on('stateChange', function(state) {
   console.log('state changed to ' + state);
   if(state == 'poweredOn') {
     var name = 'name';
     var serviceUuids = ['fffffffffffffffffffffffffffffff0']
     bleno.startAdvertising(name, serviceUuids, function(error) {
-      console.error('advertising callback: ' + error);
+        console.error('advertising callback: ' + error);
+        if (!error) {
+            bleno.setServices([
+                new BlenoPrimaryService({
+                    uuid: 'fffffffffffffffffffffffffffffff0',
+                    characteristics: [
+                        new Characteristic()
+                    ]
+                })
+            ]);
+        }
     });
     console.log('started advertising');
   }
 });
-
-
