3ba4bb6031abb93e0fcca1693cef8d2b0270c66b
marcin93w <marcin93w@gmail.com>
2016-06-04 15:51:30 +0200

    gfsdf

--- index.js
+++ index.js
@@ -60,95 +60,95 @@ function explore(peripheral) {
   });
 
   peripheral.connect(function(error) {
-    peripheral.discoverServices([], function(error, services) {
-      var serviceIndex = 0;
+    // peripheral.discoverServices([], function(error, services) {
+    //   var serviceIndex = 0;
 
-      async.whilst(
-        function () {
-          return (serviceIndex < services.length);
-        },
-        function(callback) {
-          var service = services[serviceIndex];
-          var serviceInfo = service.uuid;
+    //   async.whilst(
+    //     function () {
+    //       return (serviceIndex < services.length);
+    //     },
+    //     function(callback) {
+    //       var service = services[serviceIndex];
+    //       var serviceInfo = service.uuid;
 
-          if (service.name) {
-            serviceInfo += ' (' + service.name + ')';
-          }
-          console.log(serviceInfo);
+    //       if (service.name) {
+    //         serviceInfo += ' (' + service.name + ')';
+    //       }
+    //       console.log(serviceInfo);
 
-          service.discoverCharacteristics([], function(error, characteristics) {
-            var characteristicIndex = 0;
+    //       service.discoverCharacteristics([], function(error, characteristics) {
+    //         var characteristicIndex = 0;
 
-            async.whilst(
-              function () {
-                return (characteristicIndex < characteristics.length);
-              },
-              function(callback) {
-                var characteristic = characteristics[characteristicIndex];
-                var characteristicInfo = '  ' + characteristic.uuid;
+    //         async.whilst(
+    //           function () {
+    //             return (characteristicIndex < characteristics.length);
+    //           },
+    //           function(callback) {
+    //             var characteristic = characteristics[characteristicIndex];
+    //             var characteristicInfo = '  ' + characteristic.uuid;
 
-                if (characteristic.name) {
-                  characteristicInfo += ' (' + characteristic.name + ')';
-                }
+    //             if (characteristic.name) {
+    //               characteristicInfo += ' (' + characteristic.name + ')';
+    //             }
 
-                async.series([
-                  function(callback) {
-                    characteristic.discoverDescriptors(function(error, descriptors) {
-                      async.detect(
-                        descriptors,
-                        function(descriptor, callback) {
-                          return callback(descriptor.uuid === '2901');
-                        },
-                        function(userDescriptionDescriptor){
-                          if (userDescriptionDescriptor) {
-                            userDescriptionDescriptor.readValue(function(error, data) {
-                              if (data) {
-                                characteristicInfo += ' (' + data.toString() + ')';
-                              }
-                              callback();
-                            });
-                          } else {
-                            callback();
-                          }
-                        }
-                      );
-                    });
-                  },
-                  function(callback) {
-                        characteristicInfo += '\n    properties  ' + characteristic.properties.join(', ');
+    //             async.series([
+    //               function(callback) {
+    //                 characteristic.discoverDescriptors(function(error, descriptors) {
+    //                   async.detect(
+    //                     descriptors,
+    //                     function(descriptor, callback) {
+    //                       return callback(descriptor.uuid === '2901');
+    //                     },
+    //                     function(userDescriptionDescriptor){
+    //                       if (userDescriptionDescriptor) {
+    //                         userDescriptionDescriptor.readValue(function(error, data) {
+    //                           if (data) {
+    //                             characteristicInfo += ' (' + data.toString() + ')';
+    //                           }
+    //                           callback();
+    //                         });
+    //                       } else {
+    //                         callback();
+    //                       }
+    //                     }
+    //                   );
+    //                 });
+    //               },
+    //               function(callback) {
+    //                     characteristicInfo += '\n    properties  ' + characteristic.properties.join(', ');
 
-                    if (characteristic.properties.indexOf('read') !== -1) {
-                      characteristic.read(function(error, data) {
-                        if (data) {
-                          var string = data.toString('ascii');
+    //                 if (characteristic.properties.indexOf('read') !== -1) {
+    //                   characteristic.read(function(error, data) {
+    //                     if (data) {
+    //                       var string = data.toString('ascii');
 
-                          characteristicInfo += '\n    value       ' + data.toString('hex') + ' | \'' + string + '\'';
-                        }
-                        callback();
-                      });
-                    } else {
-                      callback();
-                    }
-                  },
-                  function() {
-                    console.log(characteristicInfo);
-                    characteristicIndex++;
-                    callback();
-                  }
-                ]);
-              },
-              function(error) {
-                serviceIndex++;
-                callback();
-              }
-            );
-          });
-        },
-        function (err) {
-          peripheral.disconnect();
-        }
-      );
-    });
+    //                       characteristicInfo += '\n    value       ' + data.toString('hex') + ' | \'' + string + '\'';
+    //                     }
+    //                     callback();
+    //                   });
+    //                 } else {
+    //                   callback();
+    //                 }
+    //               },
+    //               function() {
+    //                 console.log(characteristicInfo);
+    //                 characteristicIndex++;
+    //                 callback();
+    //               }
+    //             ]);
+    //           },
+    //           function(error) {
+    //             serviceIndex++;
+    //             callback();
+    //           }
+    //         );
+    //       });
+    //     },
+    //     function (err) {
+    //       peripheral.disconnect();
+    //     }
+    //   );
+    //});
   });
 }
 
