64dd9afdc0da5a7d926716cb0e7ac5897b445a0a
marcin93w <marcin93w@gmail.com>
2016-06-04 17:22:30 +0200

    sad

--- index.js
+++ index.js
@@ -73,98 +73,30 @@ function explore(peripheral) {
 
   peripheral.connect(function(error) {
     peripheral.discoverServices([], function(error, services) {
-      var serviceIndex = 0;
 
-      async.whilst(
-        function () {
-          return (serviceIndex < services.length);
-        },
-        function(callback) {
-          var service = services[serviceIndex];
-          var serviceInfo = service.uuid;
+      for(service in services) {
+        console.log(service.uuid + ' ' + service.name);
 
-          if (service.name) {
-            serviceInfo += ' (' + service.name + ')';
+        service.discoverCharacteristics([], function(error, characteristics) {
+          for(characteristic in characteristics) {
+            if(error) {
+              console.log('Characteristic error: ' + error);
+            } else {
+              console.log(characteristic.uuid + ' ' + characteristic.name);
+            }
           }
-          console.log(serviceInfo);
+        });
 
-          service.discoverCharacteristics([], function(error, characteristics) {
-            var characteristicIndex = 0;
+      
 
-            async.whilst(
-              function () {
-                return (characteristicIndex < characteristics.length);
-              },
-              function(callback) {
-                var characteristic = characteristics[characteristicIndex];
-                var characteristicInfo = '  ' + characteristic.uuid;
-
-                if (characteristic.name) {
-                  characteristicInfo += ' (' + characteristic.name + ')';
-                }
-
-                async.series([
-                  function(callback) {
-                    characteristic.discoverDescriptors(function(error, descriptors) {
-                      async.detect(
-                        descriptors,
-                        function(descriptor, callback) {
-                          return callback(descriptor.uuid === '2901');
-                        },
-                        function(userDescriptionDescriptor){
-                          if (userDescriptionDescriptor) {
-                            userDescriptionDescriptor.readValue(function(error, data) {
-                              if (data) {
-                                characteristicInfo += ' (' + data.toString() + ')';
-                              }
-                              callback();
-                            });
-                          } else {
-                            callback();
-                          }
-                        }
-                      );
-                    });
-                  },
-                  function(callback) {
-                        characteristicInfo += '\n    properties  ' + characteristic.properties.join(', ');
-
-                    if (characteristic.properties.indexOf('read') !== -1) {
-                      characteristic.read(function(error, data) {
-                        if (data) {
-                          var string = data.toString('ascii');
-
-                          characteristicInfo += '\n    value       ' + data.toString('hex') + ' | \'' + string + '\'';
-                        }
-                        callback();
-                      });
-                    } else {
-                      callback();
-                    }
-                  },
-                  function() {
-                    console.log(characteristicInfo);
-                    characteristicIndex++;
-                    callback();
-                  }
-                ]);
-              },
-              function(error) {
-                if(error)
-                  console.log('Characteristic error: ' + error);
-                serviceIndex++;
-                callback();
-              }
-            );
-          });
-        },
+    }
+  },
         function (err) {
           console.log('Disconnecting ' + err);
           peripheral.disconnect();
         }
       );
     });
-  });
 }
 
 function createNewProcess(ignored) {
