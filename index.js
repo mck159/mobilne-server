5d074b96ba1da69b8a6ed53e4909adddefbae04a
Maciej Sroka <mck159@gmail.com>
2016-06-26 16:37:11 +0000

    changes

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 var async = require('async');
 var noble = require('noble');
+var _ = require('underscore');
 
-var ignoreuuids = process.argv[2] || '';
+var restClient = require('./restClient.js');
+
+var ignoredIdsParam = process.argv[2] || '';
+var specificIdParam = process.argv[3];
+
+var connectedDevices = [];
 
 function getIgnored() {
-  return ignoreuuids.split(',');
+  return ignoredIdsParam.split(',');
 }
 
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
-    console.log('Started scanning');
     noble.startScanning();
   } else {
     console.log('Stopped scanning');
@@ -21,12 +26,12 @@ noble.on('warning', function(msg) {
   console.log('warning ' + msg);
 });
 noble.on('discover', function(peripheral) {
-    console.log('Discovered');
 
-    if(getIgnored().some(id => id == peripheral.id)) {
+    if(specificIdParam && specificIdParam != peripheral.id) {
+      return;
+    } else if(getIgnored().some(id => id == peripheral.id)) {
       return;
     }
-    
     console.log('peripheral with ID ' + peripheral.id + ' found');
 
     var advertisement = peripheral.advertisement;
@@ -37,76 +42,90 @@ noble.on('discover', function(peripheral) {
     var serviceData = advertisement.serviceData;
     var serviceUuids = advertisement.serviceUuids;
 
-    if (localName) {
-      console.log('  Local Name        = ' + localName);
-    }
-
-    if (txPowerLevel) {
-      console.log('  TX Power Level    = ' + txPowerLevel);
-    }
-
-    if (manufacturerData) {
-      console.log('  Manufacturer Data = ' + manufacturerData.toString('hex'));
-    }
-
-    if (serviceData) {
-      console.log('  Service Data      = ' + serviceData);
-    }
-
-    if (serviceUuids) {
-      console.log('  Service UUIDs     = ' + serviceUuids);
-    }
-
     console.log();
 
     explore(peripheral);
     
-    createNewProcess(peripheral.id);
+    createNewProcessIgnorigPeripheral(peripheral.id);
 });
 
 function explore(peripheral) {
-  console.log('services and characteristics:');
-
   peripheral.on('disconnect', function() {
     console.log(peripheral.id + ' disconnected');
+    restClient.sendDisconnectedInfo(peripheral.id);
+    createNewProcessForSpecificPeripheral(peripheral.id);
   });
 
   peripheral.connect(function(error) {
-    peripheral.discoverServices([], function(error, services) {
-
-      for(service in services) {
-        console.log(service.uuid + ' ' + service.name);
-
-        service.discoverCharacteristics([], function(error, characteristics) {
-          for(characteristic in characteristics) {
-            if(error) {
-              console.log('Characteristic error: ' + error);
-            } else {
-              console.log(characteristic.uuid + ' ' + characteristic.name);
-            }
-          }
-        });
-
-      
-
+    if(error) {
+      console.log(peripheral.id + ' connecting error' + error);
+      return;
     }
-  },
-        function (err) {
-          console.log('Disconnecting ' + err);
-          peripheral.disconnect();
+    console.log(peripheral.id + ' connected');
+    peripheral.discoverAllServicesAndCharacteristics(function(error, services, characteristics) {
+        if(error) {
+          console.log('Error: ' + error);
         }
-      );
-    });
+        
+        var deviceInfo = {
+          uuid: peripheral.id
+        };
+        getDataForCharacteristic(characteristics, '2a00', deviceName => {
+          deviceInfo.peripheralName = deviceName;
+          console.log('Device name: ' + deviceName);
+          getDataForCharacteristic(characteristics, '00002a1900001000800000805f9b34f3', value => { 
+            var values = value.split(':');
+            deviceInfo.peripheralManufacturer = values[0];
+            deviceInfo.peripheralModel = values[1];
+            console.log('Custom: ' + value);
+            
+            getDataForCharacteristic(characteristics, '00002a1900001000800000805f9b34f7', value => {
+              deviceInfo.peripheralIMEI = value;
+              console.log('Custom: ' + value);
+              restClient.sendConnectedInfo(deviceInfo);
+            
+            });
+          });
+          
+        });
+        
+      },
+      function (err) {
+        console.log('Disconnecting ' + err);
+        peripheral.disconnect();
+      });
+  });
 }
 
-function createNewProcess(ignored) {
-  ignoreuuids += ',' + ignored;
+function getDataForCharacteristic(characteristics, characteristicId, callback) {
+  var characteristic = _.find(characteristics, charact => charact.uuid.toString() == characteristicId);
+  if(characteristic) {
+    characteristic.read(function(error, data) {
+        callback(data.toString());
+    });
+  }
+}
+
+function createNewProcessIgnorigPeripheral(ignored) {
+  createNewProcess(ignored, null);
+}
+
+function createNewProcessForSpecificPeripheral(specific) {
+  createNewProcess(null, specific);
+}
+
+function createNewProcess(ignored, specific) {
+  if(ignored) {
+    ignoredIdsParam += ',' + ignored;
+  }
+  
+  //console.log('Create process with args ' + ignoredIdsParam);
   
   const spawn = require('child_process').spawn;
-  const ls = spawn('node', ['index.js', ignoreuuids]);
+  const ls = spawn('node', ['index.js', ignoredIdsParam, specific]);
 
   ls.stdout.on('data', (data) => {
-    console.log(data);
+    console.log(`${data}`);
   });
 
   ls.stderr.on('data', (data) => {
---
+++ restClient.js
@@ -0,0 +1,45 @@
+var http = require('http');
+var _ = require('underscore');
+
+var exports = module.exports = {};
+
+var _url = 'http://192.168.137.58';
+
+var connectedDevices = [];
+
+exports.sendConnectedInfo = function(deviceInfo) {
+  console.log('Sending connected info: ' + JSON.stringify(deviceInfo));
+  connectedDevices.push(deviceInfo);
+  sendData('/peripheralData/connected', deviceInfo);
+};
+
+exports.sendDisconnectedInfo = function(deviceId) {
+  var device = _.find(connectedDevices, dev => dev.uuid == deviceId);
+  if(device) {
+    console.log('Sending disconnected info: ' + deviceId);
+    sendData('/peripheralData/disconnected', device);
+  }
+};
+
+function sendData(path, jsonData) {
+  var options = {
+    host: _url,
+    port: 8080,
+    path: path,
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(jsonData)
+    }
+  };
+
+  var req = http.request(options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+          console.log("body: " + chunk);
+      });
+  });
+
+  req.write(JSON.stringify(jsonData));
+  req.end();
+}
