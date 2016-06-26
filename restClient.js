31ed284310447745dd4433810cdfcca46b5db966
Maciej Sroka <mck159@gmail.com>
2016-06-26 17:35:07 +0000

    Changed library for http requests

--- restClient.js
+++ restClient.js
@@ -1,5 +1,5 @@
-var http = require('http');
 var _ = require('underscore');
+var request = require('request');
 
 var exports = module.exports = {};
 
@@ -22,24 +22,7 @@ exports.sendDisconnectedInfo = function(deviceId) {
 };
 
 function sendData(path, jsonData) {
-  var options = {
-    host: _url,
-    port: 443,
-    path: path,
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(jsonData)
-    }
-  };
-
-  var req = http.request(options, function(res) {
-      res.setEncoding('utf8');
-      res.on('data', function (chunk) {
-          console.log("body: " + chunk);
-      });
+  request.post(_url + path, { json: jsonData }, function(error, response, body) {
+    console.log("response: " + response.statusCode + " body: " + body);
   });
-
-  req.write(JSON.stringify(jsonData));
-  req.end();
 }
